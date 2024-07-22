a8f60e825c5b59bbebc2193e8b6a058b800433df
Sourav Mazumdar <sourav.mozu@gmail.com>
2024-07-22 22:22:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function firstWord(s) {
   // your code here
+	return s.split(' ')[0]
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s));
+alert(firstWord(s))
