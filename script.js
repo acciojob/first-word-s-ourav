5e7322c2a7a689c6a311ae819cb3c5245511e2e6
Sourav Mazumdar <sourav.mozu@gmail.com>
2024-07-23 12:36:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+wrd
 function firstWord(s) {
   // your code here
-	return s.split(' ')[0]
+	wrd= s.split(' ')[0]
 }
-
+for(w in wrd){
+	if(w!=' ')
+		return w
+}
 // Do not change the code below
 
-const s = prompt("Enter String:");
+const s = prompt("Enter String:")
 alert(firstWord(s))
